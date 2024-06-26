f2d698a23524c0bf7e3ddb34f3a2ec587ba8acfd
Fortland2018(Wiktor_Zabka) <adamkowalskipl68@gmail.com>
2024-06-26 19:14:27 +0200

    ikony i plansza oraz początek wgrywania ich/lokowania, godzina i link do filmiku

---
+++ start.py
@@ -0,0 +1,74 @@
+import pygame
+
+pygame.init()
+WIDTH = 1000
+HIGHT = 800
+screen = pygame.display.set_mode((WIDTH, HIGHT))
+pygame.display.set_caption('Szachy Wiktora Zabki')
+font = pygame.font.Font('freesansbold.ttf', 20)
+big_font = pygame.font.Font('freesansbold.ttf', 50)
+
+timer  = pygame.time.Clock()
+fps = 60
+
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('dark gray')
+
+    for event in pygame.event.get():
+        if event.type ==  pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()
+
+
+# Wczytywanie i skalowanie czarnych figur
+black_queen = pygame.image.load('assets/images/bQ.svg')
+black_queen = pygame.transform.scale(black_queen, (80, 80))
+black_queen_small = pygame.transform.scale(black_queen, (45, 45))
+black_king = pygame.image.load('assets/images/bK.svg')
+black_king = pygame.transform.scale(black_king, (80, 80))
+black_king_small = pygame.transform.scale(black_king, (45, 45))
+black_rook = pygame.image.load('assets/images/bR.svg')
+black_rook = pygame.transform.scale(black_rook, (80, 80))
+black_rook_small = pygame.transform.scale(black_rook, (45, 45))
+black_bishop = pygame.image.load('assets/images/bB.svg')
+black_bishop = pygame.transform.scale(black_bishop, (80, 80))
+black_bishop_small = pygame.transform.scale(black_bishop, (45, 45))
+black_knight = pygame.image.load('assets/images/bN.svg')
+black_knight = pygame.transform.scale(black_knight, (80, 80))
+black_knight_small = pygame.transform.scale(black_knight, (45, 45))
+black_pawn = pygame.image.load('assets/images/bP.svg')
+black_pawn = pygame.transform.scale(black_pawn, (65, 65))
+black_pawn_small = pygame.transform.scale(black_pawn, (45, 45))
+
+# Wczytywanie i skalowanie białych figur
+white_queen = pygame.image.load('assets/images/wQ.svg')
+white_queen = pygame.transform.scale(white_queen, (80, 80))
+white_queen_small = pygame.transform.scale(white_queen, (45, 45))
+white_king = pygame.image.load('assets/images/wK.svg')
+white_king = pygame.transform.scale(white_king, (80, 80))
+white_king_small = pygame.transform.scale(white_king, (45, 45))
+white_rook = pygame.image.load('assets/images/wR.svg')
+white_rook = pygame.transform.scale(white_rook, (80, 80))
+white_rook_small = pygame.transform.scale(white_rook, (45, 45))
+white_bishop = pygame.image.load('assets/images/wB.svg')
+white_bishop = pygame.transform.scale(white_bishop, (80, 80))
+white_bishop_small = pygame.transform.scale(white_bishop, (45, 45))
+white_knight = pygame.image.load('assets/images/wN.svg')
+white_knight = pygame.transform.scale(white_knight, (80, 80))
+white_knight_small = pygame.transform.scale(white_knight, (45, 45))
+white_pawn = pygame.image.load('assets/images/wP.svg')
+white_pawn = pygame.transform.scale(white_pawn, (65, 65))
+white_pawn_small = pygame.transform.scale(white_pawn, (45, 45))
+
+# Listy figur
+white_images = [white_pawn, white_queen, white_king, white_knight, white_rook, white_bishop]
+small_white_images = [white_pawn_small, white_queen_small, white_king_small, white_knight_small,
+                      white_rook_small, white_bishop_small]
+black_images = [black_pawn, black_queen, black_king, black_knight, black_rook, black_bishop]
+small_black_images = [black_pawn_small, black_queen_small, black_king_small, black_knight_small,
+                      black_rook_small, black_bishop_small]
+piece_list = ['pawn', 'queen', 'king', 'knight', 'rook', 'bishop']
